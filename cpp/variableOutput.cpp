80f4e9700e1cab3bb232cedfdfdb8047e46154b9
Prescott J. Traversi <ptraversi@gmail.com>
2021-01-14 04:06:05 -0800

    feat: multi line versus inline variable output

--- cpp/variableOutput.cpp
+++ cpp/variableOutput.cpp
@@ -7,11 +7,14 @@ int main() {
   hour = 8;
   minute = 33;
 
+  // multi line
   std::cout << "The current times is ";
   std::cout << hour;
   std::cout << colon;
   std::cout << minute;
   std::cout << std::endl;
+  // single line
+  std::cout << "The current time is " << hour << colon << minute << std::endl;
 
   return 0;
 }
