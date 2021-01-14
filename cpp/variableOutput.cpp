020115e63f77fc695b91c3bc95d95833ebf42799
Prescott J. Traversi <ptraversi@gmail.com>
2021-01-14 03:50:31 -0800

    feat: multi variable output

---
+++ cpp/variableOutput.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+  int hour, minute;
+  char colon = ':';
+
+  hour = 8;
+  minute = 33;
+
+  std::cout << "The current times is ";
+  std::cout << hour;
+  std::cout << colon;
+  std::cout << minute;
+  std::cout << std::endl;
+
+  return 0;
+}
