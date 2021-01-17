f7238984014586cdd950d2767cae4d3a198f65a2
Prescott J. Traversi <ptraversi@gmail.com>
2021-01-17 05:11:09 -0800

    feat: add validation / testing

--- cpp/distance.cpp
+++ cpp/distance.cpp
@@ -5,8 +5,40 @@ int Displacement(int x0, int xf) {
   return xf - x0;
 }
 
+int ValidateDisplacement() {
+  int res = Displacement(0, 5);
+  if (res != 5) {
+    return 0;
+  }
+  res = Displacement(6, 0);
+  if (res != -6) {
+    return 0;
+  }
+  res = Displacement(3, 3);
+  if (res != 0) {
+    return 0;
+  }
+  return 1;
+}
+
 int Square(int val) {
   return val * val;
+}
+
+int ValidateSquare() {
+  int res = Square(1);
+  if (res != 1) {
+    return 0;
+  }
+  res = Square(2);
+  if (res != 4) {
+    return 0;
+  }
+  res = Square(-6);
+  if (res != 36) {
+    return 0;
+  }
+  return 1;
 }
 
 int PythagoreanTheorem(int a, int b) {
@@ -18,6 +50,22 @@ int PythagoreanTheorem(int a, int b) {
   return c;
 }
 
+int ValidatePythagoreanTheorem() {
+  int res = PythagoreanTheorem(3, 4);
+  if (res != 5) {
+    return 0;
+  }
+  res = PythagoreanTheorem(4, -4);
+  if (res != 5) {
+    return 0;
+  }
+  res = PythagoreanTheorem(1, 4);
+  if (res != 4) {
+    return 0;
+  }
+  return 1;
+}
+
 int CalcDistance(int x1,int x2,int y1,int y2) {
   int dx, dy, distance;
   dx = Displacement(x1, x2);
@@ -27,8 +75,14 @@ int CalcDistance(int x1,int x2,int y1,int y2) {
 }
 
 int main() {
-  int distance = CalcDistance(1, 6, 2, 3);
-  std::cout << distance << std::endl;
+  int d, sq, pt;
+  d = ValidateDisplacement();
+  sq =ValidateSquare();
+  pt =ValidatePythagoreanTheorem();
 
+  if (d && sq && pt) {
+    int distance = CalcDistance(1, 6, 2, 3);
+    std::cout << distance << std::endl;
+  }
   return 0;
 }
