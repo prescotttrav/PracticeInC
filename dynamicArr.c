0ff4edda1c4016fce79a8336b2dc493c7ebc9210
Prescott J. Traversi <ptraversi@gmail.com>
2021-05-18 13:22:19 -0700

    feat: dynamic array delete by index & value

--- dynamicArr.c
+++ dynamicArr.c
@@ -1,4 +1,3 @@
-// TODO delete & delete val
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -6,16 +5,26 @@
 
 struct arr {
   _Bool (*add)(struct arr *, int, int);
+  _Bool (*delete)(struct arr *, int);
+  _Bool (*delete_val)(struct arr *, int);
   int (*find_val)(struct arr *, int val);
   void (*print)(struct arr *);
   int size;
   int *items;
 };
 
+static void free_dynamic_arr(struct arr a) {
+  free(a.items);
+}
+
+static _Bool is_invalid_index(int i) {
+  return i < 0;
+}
+
 static _Bool add_arr(struct arr *a, int i, int val) {
   int *temp = NULL;
 
-  if (a->size < i)
+  if (a->size < i || is_invalid_index(i))
     return 0;
 
   a->size++;
@@ -52,9 +61,42 @@ static void print_arr(struct arr *a) {
   putchar('\n');
 }
 
+static _Bool delete_arr(struct arr *a, int i) {
+  int *temp = NULL;
+
+  if (a->size < i || is_invalid_index(i))
+    return 0;
+
+  temp = a->items;
+
+  a->items = malloc(sizeof(int) * a->size - 1);
+
+  for (int j = 0; j < a->size; j++) {
+    if (j < i) {
+      a->items[j] = temp[j];
+    } else if (j > i) {
+      a->items[j - 1] = temp[j];
+    } else {
+      continue;
+    }
+  }
+
+  a->size--;
+  free(temp);
+
+  return 1;
+}
+
+static _Bool delete_val_arr(struct arr *a, int val) {
+  int i = find_val_arr(a, val);
+  return delete_arr(a, i);
+}
+
 static struct arr init_arr(void) {
   struct arr a;
   a.add = add_arr;
+  a.delete = delete_arr;
+  a.delete_val = delete_val_arr;
   a.find_val = find_val_arr;
   a.print = print_arr;
   a.size = 0;
@@ -79,6 +121,14 @@ int main(void) {
   assert(a.find_val(&a, 2) == 1);
   assert(a.find_val(&a, 3) == 2);
   assert(a.find_val(&a, 4) == -1);
+
+  a.delete(&a, 1);
+  a.print(&a);
+
+  a.delete_val(&a, 3);
+  a.print(&a);
+
+  free_dynamic_arr(a);
 
   return 0;
 }
