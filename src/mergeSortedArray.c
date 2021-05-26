b1d7377d1150127318bd108f69d8908b3abbe612
Prescott J. Traversi <ptraversi@gmail.com>
2021-05-26 08:05:46 -0700

    fix: code simplification

--- src/mergeSortedArray.c
+++ src/mergeSortedArray.c
@@ -13,20 +13,18 @@
 
 /* --------------------------------- problem -------------------------------- */
 
-static _Bool isGreater(int x, int y) {
-  return x > y;
-}
-
 void merge(int *nums1, int nums1Size, int m, int *nums2, int nums2Size, int n) {
   int i = m - 1;
   int j = n - 1;
   int k = m + n - 1;
-  while (k >= 0) {
-    if (i >= 0 && (j < 0 || isGreater(nums1[i], nums2[j])))
+  while (i >= 0 && j >= 0) {
+    if (nums1[i] > nums2[j])
       nums1[k--] = nums1[i--];
     else
       nums1[k--] = nums2[j--];
   }
+  while (j >= 0)
+    nums1[k--] = nums2[j--];
 }
 
 /* --------------------------------- testing -------------------------------- */
