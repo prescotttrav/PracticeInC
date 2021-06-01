953239d1e2a405719044fdc4eafbf5ffc74e6f7d
Prescott J. Traversi <ptraversi@gmail.com>
2021-06-01 05:19:31 -0700

    feat: working initial commit

---
+++ src/majorityElement.c
@@ -0,0 +1,20 @@
+/* Author: Prescott J. Traversi
+ * Source: https://leetcode.com/problems/majority-element/
+ *
+ * Given an array nums of size n, return the majority element.
+ *
+ * Time: O()
+ * Space: O()
+ */
+#include <stdlib.h>
+
+#include <assert.h>
+
+/* --------------------------------- problem -------------------------------- */
+
+// First hashmap
+// Second div & conquer
+// Third randomized algo
+// Fourth Boyer-Moore Voting Algorithm
+
+/* --------------------------------- testing -------------------------------- */
